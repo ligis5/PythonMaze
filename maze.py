ce153a1705483b7a9efa2904ce7a2722d2bf7dbc
lygis <lukasnor5@gmail.com>
2025-03-09 12:25:47 +0200

    Changed 2d array to hold dictionaries inside now, with coordinate and True or False statement, which will be used to draw or not draw a wall.

--- maze.py
+++ maze.py
@@ -17,12 +17,12 @@ def grid_array():
     i = 0
     x = []
     while i < SCREEN_WIDTH:
-        x.append(i)
+        x.append({"x":i,"exist": True})
         i+=grid_density
     j = 0
     y = []
     while j < SCREEN_HEIGHT:
-        y.append(j)
+        y.append({"y":j,"exist": True})
         j += grid_density
 
     x_array = np.array(x)
@@ -30,19 +30,22 @@ def grid_array():
     X, Y = np.meshgrid(x_array, y_array)
     return X, Y
 
-
+# def mazify_grid():
+#
+#     pass
 
 def draw_cells(grid):
         for j in range(len(grid[0])):
             for x in range(len(grid[0][0])):
                 for y in range(len(grid[1][0])):
                     # X axis lines
-                    pygame.draw.line(screen, (60, 60, 60), (grid[0][j][x], grid[1][j][y]), (grid[0][j][x] + grid_density, grid[1][j][y]),2)
+                    pygame.draw.line(screen, (60, 60, 60), (grid[0][j][x]["x"], grid[1][j][y]["y"]), (grid[0][j][x]["x"] + grid_density, grid[1][j][y]["y"]),2)
                     # Y axis lines
-                    pygame.draw.line(screen, (60, 60, 60), (grid[0][j][y], grid[1][j][x]),
-                                     (grid[0][j][y], grid[1][j][x] + grid_density), 2)
+                    pygame.draw.line(screen, (60, 60, 60), (grid[0][j][y]["x"], grid[1][j][x]["y"]),
+                                     (grid[0][j][y]["x"], grid[1][j][x]["y"] + grid_density), 2)
 
 loop = True
+
 
 grid = grid_array()
 
